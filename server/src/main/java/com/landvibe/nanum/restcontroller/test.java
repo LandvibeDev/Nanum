94eb7350e220c2ea011aec068758d2727861db6e
GuBeomJeong <jgb625@naver.com>
2017-12-28 14:23:43 +0900

    setting server structure

---
+++ server/src/main/java/com/landvibe/nanum/restcontroller/test.java
@@ -0,0 +1,14 @@
+package com.landvibe.nanum.restcontroller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class test {
+    @GetMapping("/")
+    String home() {
+        return "Hello World !";
+    }
+
+
+}
