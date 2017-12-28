ce28f7b107a1062be688c9d7623c1c644b089658
GuBeomJeong <jgb625@naver.com>
2017-12-28 15:56:51 +0900

    Update gitignore out/

--- server/src/main/java/com/landvibe/nanum/config/WebConfig.java
+++ server/src/main/java/com/landvibe/nanum/config/WebConfig.java
@@ -1,16 +1,11 @@
 package com.landvibe.nanum.config;
 
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
-    //---중략---/
-
-    //static 리소스 처리
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 //        registry.addResourceHandler("/resources/**")
