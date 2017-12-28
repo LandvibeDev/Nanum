7f6b4e39f66f13b809642b6319379e4850dbc958
GuBeomJeong <jgb625@naver.com>
2017-12-28 16:27:39 +0900

    Update resource path

--- server/src/main/java/com/landvibe/nanum/config/WebConfig.java
+++ server/src/main/java/com/landvibe/nanum/config/WebConfig.java
@@ -1,15 +1,22 @@
 package com.landvibe.nanum.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
 public class WebConfig extends WebMvcConfigurerAdapter {
+
+
+    @Value("${spring.resources.static-locations}")
+    private String staticResourceLocation;
+
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
-//        registry.addResourceHandler("/resources/**")
-//                .addResourceLocations("/WEB-INF/resources/");
+        registry.addResourceHandler("/resources/**")
+                .addResourceLocations(staticResourceLocation);
 
         registry.addResourceHandler("swagger-ui.html")
                 .addResourceLocations("classpath:/META-INF/resources/");
