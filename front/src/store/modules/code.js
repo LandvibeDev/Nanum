495f476986417dc5a2970a42aaf6c90947208fa5
GuBeomJeong <jgb625@naver.com>
2018-02-06 18:35:25 +0900

    Add vuex structure

---
+++ front/src/store/modules/code.js
@@ -0,0 +1,36 @@
+import axios from 'axios'
+
+const state = {
+  dragFile: null
+}
+
+const getters = {
+  dragFile: function (state) {
+    return state.dragFile
+  }
+}
+
+const actions = {
+  moveDragFile: function ({commit}, {projectId, fileName, path, newPath, fileType}) {
+    let url = '/api/projects/' + projectId + '/moveFiles/' + fileName
+    const data = {
+      path: path,
+      newPath: newPath,
+      type: fileType
+    }
+    return axios.put(url, data)
+  }
+}
+
+const mutations = {
+  setDragFile (state, dragFile) {
+    state.dragFile = dragFile
+  }
+}
+
+export default {
+  state,
+  getters,
+  actions,
+  mutations
+}
